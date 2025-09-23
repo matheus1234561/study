dfea44f990004635f8fc3d726b20ed4af9f130e5
matheus.romero <matheus.hoshigaki@hotmail.com>
2025-09-23 19:09:26 -0300

    Commit: Adição de styles e alterações na estrutura da página que lista usuários.

--- node-study/frontend-docker/src/features/user/pages/list-user/list-user.tsx
+++ node-study/frontend-docker/src/features/user/pages/list-user/list-user.tsx
@@ -1,13 +1,14 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import { useState } from "react";
 import { PageContent } from "../../../../shared/components/PageContent/page-content";
 import { DateFormatter } from "../../../../shared/utils/formatter";
 import { User } from "../../../../shared/models/user-model";
 import { getUsers } from "../../../../service/user-service";
+import { Delete, Edit } from "@mui/icons-material";
+import listUserStyle from "./list-user.module.css";
 
 export const ListUser = () => {
   const [users, setUsers] = useState<User[]>([]);
-
 
   const listUser = () => {
     const user = getUsers();
@@ -18,7 +19,7 @@ export const ListUser = () => {
         console.error("Unexpected response:", res);
       }
     });
-  }
+  };
 
   return (
     <PageContent title="Lista de Usuários">
@@ -33,19 +34,22 @@ export const ListUser = () => {
       <Box>
         {users.length > 0 ? (
           users.map((user) => (
-            <Box
-              key={user.id}
-              className="user-box"
-              mb={2}
-              p={2}
-              border={1}
-              borderRadius={2}
-            >
-              <Typography variant="h6">Name: {user.name}</Typography>
-              <Typography variant="body1">Email: {user.email}</Typography>
-              <Typography variant="body2" color="textSecondary">
-                BirthDate: {DateFormatter(user.created_at)}
-              </Typography>
+            <Box key={user.id} className={listUserStyle["user-box"]}>
+              <Box className={listUserStyle["typography-box"]}>
+                <Typography variant="h6">Name: {user.name}</Typography>
+                <Typography variant="body1">Email: {user.email}</Typography>
+                <Typography variant="body2" color="textSecondary">
+                  BirthDate: {DateFormatter(user.created_at)}
+                </Typography>
+              </Box>
+              <Box className={listUserStyle["icon-box"]}>
+                <IconButton>
+                  <Edit />
+                </IconButton>
+                <IconButton>
+                  <Delete />
+                </IconButton>
+              </Box>
             </Box>
           ))
         ) : (
--- node-study/frontend-docker/src/shared/components/PageContent/page-content.tsx
+++ node-study/frontend-docker/src/shared/components/PageContent/page-content.tsx
@@ -1,5 +1,7 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, IconButton } from "@mui/material";
 import pageContent from "./page-content.module.css";
+import { ArrowBack } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 interface PageContentProps {
   children?: React.ReactNode;
@@ -12,12 +14,24 @@ export const PageContent: React.FC<PageContentProps> = ({
   title,
   subTitle,
 }) => {
+
+  const navigate = useNavigate();
+
+  const goBack = () =>{
+    navigate(-1);
+  }
+
   return (
     <Box className={pageContent["div-page"]}>
+      <Box className={pageContent["div-header"]}>
+        <IconButton onClick={goBack}>
+        <ArrowBack className={pageContent["icon"]} fontSize="large"/>
+      </IconButton>
       <Typography className={pageContent["typography-title"]} variant="h2">
         {title}
       </Typography>
       {subTitle && <Typography>{subTitle}</Typography>}
+      </Box>
       <Box className={pageContent["div-content"]}>{children}</Box>
     </Box>
   );
